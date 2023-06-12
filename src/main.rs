a4e5bab9bb3dba52f8f879726a8a5a4160f14851
Emil Murman <emilmurman@hotmail.com>
2023-06-12 19:02:53 +0200

    bootstrap

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Error};
+use std::{env};
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn server() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args[1].as_str() {
+        "server" => {
+            server();
+        }
+        _ => println!("hello")
+    };
+}
