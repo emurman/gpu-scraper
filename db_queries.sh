feb74e358213bf1bef0e40983f4ef395847b3a73
emurman <40160022+emurman@users.noreply.github.com>
2023-06-22 20:00:58 +0200

    Update db_queries.sh

--- db_queries.sh
+++ db_queries.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker exec -i db psql -U postgres <<EOT
+docker exec -i db psql -U postgres <<SQL
 select DISTINCT ON (date::timestamp::date) date::timestamp::date, value
 FROM model, product, price
 WHERE price.product_id = product.id and product.model_id = model.id and model.name = '$1'
 ORDER BY
         date::timestamp::date,
         value ASC;
-EOT
+SQL
