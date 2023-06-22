e766e7442d6dbdc66b0453bc4c0f5c1f203279c8
Emil Murman <emil@ubuntu.lan>
2023-06-22 20:11:15 +0200

    remove old scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker image rm -f gpu-scraper-app
-nohup docker compose build > nohup.txt &
--- db.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-docker run --name scraper-db -e POSTGRES_PASSWORD=supersecret -d --publish 127.0.0.1:5432:5432 postgres:15
